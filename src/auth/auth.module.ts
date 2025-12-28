c5fdac354687bd208aea1dbacb9036d0b09bcdff
Martín Maldonado <martinmarcelomaldonado1985@gmail.com>
2025-12-28 15:37:35 -0300

    protocolo de seguridad

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,21 +2,32 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { JwtModule } from '@nestjs/jwt'; // NUEVO: Importación para habilitar JWT
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config'; // Importamos estas dos
 import { Talento, TalentoSchema } from '../talento/schemas/talento.schema'; 
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Talento.name, schema: TalentoSchema }]),
     
-    // NUEVO: Configuración del módulo de Tokens
-    JwtModule.register({
-      global: true, // Lo hace disponible en todo el proyecto
-      secret: 'PROTOCOLO_NEXUS_2025_SECRET_KEY', // Esta es la firma de seguridad
-      signOptions: { expiresIn: '1h' }, // El acceso expira en 1 hora
+    // MODIFICADO: Ahora el JwtModule se registra de forma asíncrona para leer el .env
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get<string>('JWT_SECRET'), // <-- Lee la clave del .env
+        signOptions: { expiresIn: '1h' },
+      }),
     }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
 })
 export class AuthModule { }
+
+
+
+
+
+
